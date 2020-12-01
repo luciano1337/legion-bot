5626bb49dc0cf9000c957ac81330ac5d6e89d38b
Alexandru Lucian <75133323+luciano1337@users.noreply.github.com>
2020-12-01 15:52:16 +0200

    UPDATE : Cuvinte noi , si alte variabile

--- utils/lists.py
+++ utils/lists.py
@@ -1,5 +1,48 @@
-ballresponse = [
+pozehug = [
+  'https://media1.tenor.com/images/4d89d7f963b41a416ec8a55230dab31b/tenor.gif?itemid=5166500', 
+  'https://media1.tenor.com/images/c7efda563983124a76d319813155bd8e/tenor.gif?itemid=15900664', 
+  'https://media1.tenor.com/images/daffa3b7992a08767168614178cce7d6/tenor.gif?itemid=15249774', 
+  'https://media1.tenor.com/images/7e30687977c5db417e8424979c0dfa99/tenor.gif?itemid=10522729', 
+  'https://media1.tenor.com/images/5ccc34d0e6f1dccba5b1c13f8539db77/tenor.gif?itemid=17694740'
+]
+raspunsuri = [
   'Da', 'Nu', 'Ghiceste..', 'Absolut.',
-  'Desigur.', 'Fara indoiala fratimiu.', 'Cel mai probabil.', 'Daca vreau eu', 'https://i.imgur.com/9x18D5m.png', 'Sa speram', 'Posibil.', 'Ce vorbesti sampist cordit', 'Se prea poate', 'Atata poti cumetre', 'Daca doresc', 'Teapa cumetre', 'Milsugi grav', 'https://www.youtube.com/watch?v=1MwqNFO_rM4', 'Nu stiu ca nu sunt creativa', 'Nu stiu', 'Asa te-ai nascut bai asta', 'Yamete Kudasaiii.. ^_^', 'E prost dal in morti lui!',
+  'Desigur.', 'Fara indoiala fratimiu.', 
+  'Cel mai probabil.', 'Daca vreau eu', 
+  'Ajutor acest copil are iq-ul scazut!', 
+  'https://i.imgur.com/9x18D5m.png', 
+  'Sa speram', 'Posibil.', 
+  'Ce vorbesti sampist cordit', 
+  'Se prea poate', 'Atata poti cumetre', 
+  'Daca doresc', 'Teapa cumetre', 
+  'Milsugi grav', 'https://www.youtube.com/watch?v=1MwqNFO_rM4', 
+  'Nu stiu ca nu sunt creativa', 'Nu stiu', 'Asa te-ai nascut bai asta', 'Yamete Kudasaiii.. ^_^', 'E prost dal in morti lui!',
   'Nu il poti judeca.'
+]
+lovitura = [
+  'https://media1.tenor.com/images/9ea4fb41d066737c0e3f2d626c13f230/tenor.gif?itemid=7355956', 
+  'https://media1.tenor.com/images/612e257ab87f30568a9449998d978a22/tenor.gif?itemid=16057834', 
+  'https://media1.tenor.com/images/528ff731635b64037fab0ba6b76d8830/tenor.gif?itemid=17078255', 
+  'https://media1.tenor.com/images/153b2f1bfd3c595c920ce60f1553c5f7/tenor.gif?itemid=10936993', 
+  'https://media1.tenor.com/images/f9f121a46229ea904209a07cae362b3e/tenor.gif?itemid=7859254', 
+  'https://media1.tenor.com/images/477821d58203a6786abea01d8cf1030e/tenor.gif?itemid=7958720'
+]
+pisica = [
+  'https://media1.tenor.com/images/730c85cb58041d4345404a67641fcd46/tenor.gif?itemid=4351869',
+  'https://media1.tenor.com/images/f78e68053fcaf23a6ba7fbe6b0b6cff2/tenor.gif?itemid=10614631',
+  'https://media1.tenor.com/images/8ab88b79885ab587f84cbdfbc3b87835/tenor.gif?itemid=15917800',
+  'https://media1.tenor.com/images/fea93362cd765a15b5b2f45fc6fca068/tenor.gif?itemid=14715148',
+  'https://media1.tenor.com/images/fb22e08583263754816e910f6a6ae4bd/tenor.gif?itemid=15310654',
+  'https://media1.tenor.com/images/9596d3118ddd5c600806a44da90c4863/tenor.gif?itemid=16014629',
+  'https://media1.tenor.com/images/ce038ac1010fa9514bb40d07c2dfed7b/tenor.gif?itemid=14797681',
+  'https://media1.tenor.com/images/4fbe2ab9d22992d0a42da37804f227e8/tenor.gif?itemid=9606395',
+  'https://media1.tenor.com/images/f6fe8d1d0463f4e51b6367bbecf56a3e/tenor.gif?itemid=6198981',
+  'https://media1.tenor.com/images/a862d2cb92bfbe6213e298871b1e8a9a/tenor.gif?itemid=15805236'
+]
+caini = [
+  ''
+]
+pozehentai = [
+  'https://i.alexflipnote.dev/500ce4.gif',
+
 ]
