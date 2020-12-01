2b689f1a2d11c24653ff4307ce5868e7df122d57
Alexandru Lucian <75133323+luciano1337@users.noreply.github.com>
2020-12-01 03:44:52 +0200

    UPDATE : Cuvinte noi aduse

--- utils/lists.py
+++ utils/lists.py
@@ -1,5 +1,5 @@
 ballresponse = [
   'Da', 'Nu', 'Ghiceste..', 'Absolut.',
-  'Desigur.', 'Fara indoiala fratimiu.', 'Cel mai probabil.', 'Posibil.', 'Pentru ca asa te-ai nascut bai asta', 'Yamete Kudasaiii.. ^_^', 'E prost dal in morti lui!',
+  'Desigur.', 'Fara indoiala fratimiu.', 'Cel mai probabil.', 'Daca vreau eu', 'https://i.imgur.com/9x18D5m.png', 'Sa speram', 'Posibil.', 'Ce vorbesti sampist cordit', 'Se prea poate', 'Atata poti cumetre', 'Daca doresc', 'Teapa cumetre', 'Milsugi grav', 'https://www.youtube.com/watch?v=1MwqNFO_rM4', 'Nu stiu ca nu sunt creativa', 'Nu stiu', 'Asa te-ai nascut bai asta', 'Yamete Kudasaiii.. ^_^', 'E prost dal in morti lui!',
   'Nu il poti judeca.'
 ]
